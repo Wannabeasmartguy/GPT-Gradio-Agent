60be5428e4eaa6f523d1666e8c12143cb078745a
Wannabeasmartguy <997139385@qq.com>
2023-09-13 16:11:57 +0800

    Fixed: system prompts be added repeatedly

--- GPT-Gradio-Agent.py
+++ GPT-Gradio-Agent.py
@@ -16,17 +16,22 @@ openai.api_version = "2023-07-01-preview"
 openai.api_key = AZURE_OAI_KEY
 
 
-def deliver(message, history,system,context_length:int, temperature):
+def deliver(message, history,system,context_length:int, temperature,):
+    
     if "chat_history" not in globals():
         global chat_history
         chat_history = []
-    
-        # System Prompt and User Prompt
+
+    # System Prompt and User Prompt
+    if system:
         system_input = {
             "role": "system",
             "content": system
         }
-        chat_history.append(system_input)
+        if chat_history == []:
+            chat_history.append(system_input)
+        else:
+            chat_history[0] = system_input
  
     user_input = {
         "role": "user",
@@ -56,29 +61,35 @@ def deliver(message, history,system,context_length:int, temperature):
 
     # Trim the context length first
     if len(chat_history) > context_length:
-        chat_history = chat_history[-context_length:]
+        chat_history = [chat_history[0]]+chat_history[1-context_length:]
     
     #Then return reply
     return reply
 
-# Use gr.ChatInterface for the initial build, rewrite lately
-GUI = gr.ChatInterface(
-    fn=deliver,
-    chatbot=gr.Chatbot(height=400),
-    textbox=gr.Textbox(placeholder="Ask me a question", container=False, scale=7),
-    additional_inputs=[
-        gr.Textbox("You are a helpful AI.", label="System Prompt"),
-        gr.Slider(0, 32, value=4, step=1, label="Context length"),
-        gr.Slider(0, 1, value=0.7, step=0.01, label="Temperature")
-    ],
-    title="GPT Assistance",
-    description="Test whether my GPT GUI works or not.",
-    theme="soft",
-    retry_btn="Try Again",
-    submit_btn="Submit",
-    undo_btn="Delete Previous",
-    clear_btn="Clear",
-)
+def rst_memery():
+    chat_history = []
+    return chat_history
 
-if __name__ == "__main__":
-    GUI.queue().launch(debug=True)
+with gr.Blocks() as demo:
+    gr.Markdown(
+        '''
+        # <center>GPT Assistance<center>
+        <center>Use the agent make your work and life much more efficient.<center>
+        '''
+    )
+    System_Prompt = gr.Textbox("You are a helpful AI.", label="System Prompt")
+    Context_length = gr.Slider(0, 32, value=4, step=1, label="Context length",render=False)
+    Temperature = gr.Slider(0, 1, value=0.7, step=0.01, label="Temperature",render=False)
+
+    gr.ChatInterface(
+        deliver, 
+        additional_inputs=[System_Prompt, 
+                            #clr_mem_btn,
+                            Context_length, 
+                            Temperature
+                            ],
+        theme="soft",
+        submit_btn="Submit",
+    )
+
+demo.queue().launch(debug=True)
