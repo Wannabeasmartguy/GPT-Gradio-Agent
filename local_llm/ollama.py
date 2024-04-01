c7068d349e933188df49f009b28dcd3e77af1cf1
Wannabeasmartguy <997139385@qq.com>
2024-04-01 17:18:53 +0800

    Customize Ollama Optional Params

--- local_llm/ollama.py
+++ local_llm/ollama.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 def process_chat_response(response):
     """
     处理聊天API的流式输出
@@ -17,6 +18,34 @@ def process_chat_response(response):
             yield data
 
 
+def process_api_params(is_enable:bool=False,
+                       choose_list:list=["max_tokens","frequency_penalty"],
+                       **kwargs):
+    '''
+    处理 Ollama API 传入参数
+     OpenAI 的参数和 Ollama 的参数在设置时，合适的数值大小不同
+    此外，某些参数的名称也不同，详见 https://github.com/ollama/ollama/blob/main/docs/modelfile.md#valid-parameters-and-values
+
+    Args:
+        is_enable (bool): 是否启用参数处理
+        choose_list (list): 需要保留到 API 请求的参数列表
+        kwargs (dict): 传入的参数
+    '''
+    if is_enable:
+        # 对option_kwargs进行处理，使其符合API的要求
+        num_ctx = kwargs.pop('max_tokens', None)
+        if num_ctx is not None:
+            kwargs['num_ctx'] = num_ctx
+        
+        repeat_penalty = kwargs.pop('frequency_penalty', None)
+        if repeat_penalty is not None:
+            kwargs['repeat_penalty'] = repeat_penalty
+
+        kwargs = {k: v for k, v in kwargs.items() if k in choose_list}
+
+        return kwargs
+    
+
 def send_ollama_chat_request(messages:list,
                       model:str="qwen:7b-chat", 
                       **option_kwargs
@@ -32,11 +61,14 @@ def send_ollama_chat_request(messages:list,
         dict: 服务器返回的聊天结果
     """
     url:str="http://localhost:11434/api/chat"
+
+    kwargs = process_api_params(is_enable=True, **option_kwargs)
+
     data = {
         "model": model,
         "messages": messages,
         "stream": False, # 流式输出则注释掉此行
-        "options": option_kwargs
+        "options": kwargs
     }
     response = requests.post(url, json=data, stream=True)
     # yield from process_chat_response(response) # 流式输出则取消注释此行
